9dac782de28779fc5126e633c2675d07e0ab7b8d
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-05-07 02:03:45 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,109 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = data => {
+    setContacts(prevContacts => [...prevContacts, data]);
   };
 
-  componentDidMount() {
-    const downloadContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (downloadContacts !== null) {
-      this.setState({ contacts: downloadContacts });
-      setTimeout(() => {}, 1);
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitHandler = data => {
-    this.setState(prevState => ({ contacts: [...prevState.contacts, data] }));
-    setTimeout(() => {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }, 1);
+  const filterHandler = event => {
+    setFilter(event);
   };
 
-  filterHandler = event => {
-    this.setState(() => {
-      return { filter: event };
-    });
+  const deleteContacts = id => {
+    setContacts(prevContacts => prevContacts.filter(e => e.id !== id));
+    filter === ''
+      ? setContacts(prevContacts => prevContacts.filter(e => e.id !== id))
+      : setFilter(prevFilter => prevFilter.filter(e => e.id !== id));
   };
 
-  deleteContacts = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(e => e.id !== id),
-    }));
-
-    this.state.filter === ''
-      ? this.setState(prevState => ({
-          contacts: prevState.contacts.filter(e => e.id !== id),
-        }))
-      : this.setState(prevState => ({
-          filter: prevState.filter.filter(e => e.id !== id),
-        }));
-
-    setTimeout(() => {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }, 1);
-  };
-
-  render() {
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <ContactForm
-          contacts={this.state.contacts}
-          onSubmit={this.formSubmitHandler}
-        />
-        <h2>Contacts</h2>
-        <Filter contacts={this.state.contacts} onchange={this.filterHandler} />
-        <ContactList
-          contactsList={
-            this.state.filter === '' ? this.state.contacts : this.state.filter
-          }
-          onClick={this.deleteContacts}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} onSubmit={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter contacts={contacts} onchange={filterHandler} />
+      <ContactList
+        contactsList={filter === '' ? contacts : filter}
+        onClick={deleteContacts}
+      />
+    </>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+// componentDidMount() {
+//   const downloadContacts = JSON.parse(localStorage.getItem('contacts'));
+//   if (downloadContacts !== null) {
+//     this.setState({ contacts: downloadContacts });
+//     setTimeout(() => {}, 1);
+//   }
+// }
+
+//   formSubmitHandler = data => {
+//     this.setState(prevState => ({ contacts: [...prevState.contacts, data] }));
+//     setTimeout(() => {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }, 1);
+//   };
+
+//   filterHandler = event => {
+//     this.setState(() => {
+//       return { filter: event };
+//     });
+//   };
+
+//   deleteContacts = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(e => e.id !== id),
+//     }));
+
+//     this.state.filter === ''
+//       ? this.setState(prevState => ({
+//           contacts: prevState.contacts.filter(e => e.id !== id),
+//         }))
+//       : this.setState(prevState => ({
+//           filter: prevState.filter.filter(e => e.id !== id),
+//         }));
+
+//     setTimeout(() => {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }, 1);
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Phonebook</h1>
+//         <ContactForm
+//           contacts={this.state.contacts}
+//           onSubmit={this.formSubmitHandler}
+//         />
+//         <h2>Contacts</h2>
+//         <Filter contacts={this.state.contacts} onchange={this.filterHandler} />
+//         <ContactList
+//           contactsList={
+//             this.state.filter === '' ? this.state.contacts : this.state.filter
+//           }
+//           onClick={this.deleteContacts}
+//         />
+//       </>
+//     );
+//   }
+// }
